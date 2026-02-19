00542efbb47fda7e8d5282c64c9f8da8db1b5b44
marioiordanov <mio@shardlabs.io>
2026-02-19 14:27:25 +0200

    added macos finder sync service

---
+++ macos/ArchiveClientHost/ArchiveClientHost/ArchiveClientHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveClientHostApp.swift
+//  ArchiveClientHost
+//
+//  Created by Mario Yordanov on 19.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveClientHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
