7f5d57ae1478b4eab09ed8517a2a9576802ee596
marioiordanov <mario.iordanov1995@gmail.com>
2026-06-07 00:40:18 +0300

    make file data in different places

--- src/constants.rs
+++ src/constants.rs
@@ -4,4 +4,18 @@ pub const TOKEN_URL: &str = "https://oauth2.googleapis.com/token";
 pub const FILES_URL: &str = "https://www.googleapis.com/drive/v3/files";
 pub const FILES_UPLOAD_URL: &str = "https://www.googleapis.com/upload/drive/v3/files";
 pub const ACTIVITY_URL: &str = "https://driveactivity.googleapis.com/v2/activity:query";
-pub const LOCAL_FOLDER_BASE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/app-data");
+pub fn local_folder_base() -> std::path::PathBuf {
+    #[cfg(debug_assertions)]
+    return std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("app-data");
+
+    #[cfg(not(debug_assertions))]
+    {
+        #[cfg(target_os = "macos")]
+        return std::path::PathBuf::from(std::env::var("HOME").expect("HOME not set"))
+            .join("Library/Application Support/archive-client-rs");
+
+        #[cfg(target_os = "windows")]
+        return std::path::PathBuf::from(std::env::var("APPDATA").expect("APPDATA not set"))
+            .join("archive-client-rs");
+    }
+}
